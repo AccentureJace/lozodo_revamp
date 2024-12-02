1d6209f3b2ab04759270e786b50f59919f6da032
analouMae <aquinoanalou08@gmail.com>
2024-12-02 17:23:52 +0800

    Modified: Login Page

--- src/routes/endpoints.js
+++ src/routes/endpoints.js
@@ -1,10 +1,6 @@
-import { PATH_LOGIN } from "../constants/service";
 
 export const ENDPOINTS = {
     PRODUCT_DASHBOARD : '/',
-<<<<<<< HEAD
     PATH_LOGIN : '/login',
-=======
     PRODUCT_DETAILS : '/product/:id',
->>>>>>> bdd3f5d0fbe48397942cef58e8e2c8508133cd20
 }
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,39 +1,27 @@
-<<<<<<< HEAD
 import { createBrowserRouter } from 'react-router-dom';
 import Dashboard from '../pages/user/Dashboard';
+import ProductDetails from '../pages/user/ProductDetails';
+import { StoreLayout } from '../layout';
 import { ENDPOINTS } from './endpoints';
 import Signin from '../pages/user/Signin';
 
 export const router = createBrowserRouter([
     {
-        path: ENDPOINTS.PRODUCT_DASHBOARD,
-        element: <Dashboard />,
+        path: '/',
+        element: <StoreLayout />,
+        children: [
+            {
+                path: ENDPOINTS.PRODUCT_DASHBOARD,
+                element: <Dashboard />,
+            },
+            {
+                path: ENDPOINTS.PRODUCT_DETAILS,
+                element: <ProductDetails />,
+            },
+        ],
     },
     {
         path: ENDPOINTS.PATH_LOGIN,
         element: <Signin />,
     },
-=======
-import { createBrowserRouter } from "react-router-dom";
-import Dashboard from "../pages/user/Dashboard";
-import ProductDetails from "../pages/user/ProductDetails";
-import {StoreLayout} from "../layout";
-import { ENDPOINTS } from "./endpoints";
-
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <StoreLayout />,
-    children: [
-      {
-        path: ENDPOINTS.PRODUCT_DASHBOARD,
-        element: <Dashboard />,
-      },
-      {
-        path: ENDPOINTS.PRODUCT_DETAILS,
-        element: <ProductDetails />,
-      },
-    ],
-  },
->>>>>>> bdd3f5d0fbe48397942cef58e8e2c8508133cd20
 ]);
