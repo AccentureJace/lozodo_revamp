3f578ea1c5725e4871f41a3167dd4d9a25d53259
Jason Villa <jason.villa@accenture.com>
2024-12-02 15:42:04 +0800

    deleted validation.js and added it to index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,4 @@
 import { SESSION_TOKEN } from '../constants/common';
-
 export const JWTStorage = {
 	getToken: () => {
 		return localStorage.getItem(SESSION_TOKEN);
@@ -11,4 +10,42 @@ export const JWTStorage = {
 	clearToken: () => {
 		localStorage.removeItem(SESSION_TOKEN);
 	},
+};
+
+export const validateObject = (obj) => {
+	let result = {};
+
+	const isValidPhone = (str) => {
+		const phonePattern = /09\d{9}$/;
+		return phonePattern.test(str);
+	};
+
+	const isStrongPassword = (str) => {
+		const passwordPattern = /^(?=(.*[A-Z])(?=.*?[a-z])(?=(.*[0-9]){3,})(?=.*?[#?!@$%^&*-])).{10,}$/;
+		return passwordPattern.test(str);
+	};
+
+	const isEmpty = (str) => {
+		return str == '';
+	};
+
+	for (const [key, value] of Object.entries(obj)) {
+		if (key.toLowerCase().includes('phone') || key.includes('contact')) {
+			if (!isValidPhone(value)) {
+				result[key] = 'Must be 11-digit that starts with 09';
+			}
+		}
+
+		if (key.toLowerCase().includes('password')) {
+			if (!isStrongPassword(value)) {
+				result[key] = 'Password must have atleast 10 characters, 1 special character, 3 numbers and 1 uppercase';
+			}
+		}
+
+		if (isEmpty(value)) {
+			result[key] = `This field cannot be empty`;
+		}
+	}
+
+	return result;
 };
