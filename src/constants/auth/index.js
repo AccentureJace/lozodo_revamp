0dc4b03670923a74b913edb0c94802d37ba77b18
analouMae <aquinoanalou08@gmail.com>
2024-12-05 15:48:58 +0800

    Updated' clear

--- src/constants/auth/index.js
+++ src/constants/auth/index.js
@@ -1,15 +1,15 @@
-export const CONFIRM_PASSOWRD_ERROR = 'Passwords do not match.';
-export const FIRST_NAME_MESSAGE = 'Please enter valid first name';
-export const LAST_NAME_MESSAGE = 'Please enter valid last name';
-export const CONTACT_NUMBER_MESSAGE = 'Please enter valid contact number';
-export const ADDRESS_MESSAGE = 'Please enter valid address';
-export const USERNAME_MESSAGE = 'Please enter valid username';
-export const PASSWORD_MESSAGE = 'Please enter valid password';
-export const SUCCESSFUL_SIGNUP_MESSAGE = 'User registered successfully';
-export const SUCCESSFUL_SIGNIN_MESSAGE = 'Successfully logged in!';
-export const INVALID_USERNAME_MESSAGE = 'Invalid Username';
-export const INVALID_PASSWORD_MESSAGE = 'Invalid password';
+export const VALIDATION_CONFIRM_PASSOWRD = 'Passwords do not match.';
+export const VALIDATION_FIRST_NAME = 'Please enter valid first name';
+export const VALIDATION_LAST_NAME = 'Please enter valid last name';
+export const VALIDATION_CONTACT_NUMBER = 'Please enter valid contact number';
+export const VALIDATION_ADDRESS = 'Please enter valid address';
+export const VALIDATION_USERNAME = 'Please enter valid username';
+export const VALIDATION_PASSWORD = 'Please enter valid password';
+export const SUCCESS_SIGNUP = 'User registered successfully';
+export const SUCCESS_SIGNIN = 'Successfully logged in!';
+export const INVALID_USERNAME = 'Invalid Username';
+export const INVALID_PASSWORD = 'Invalid password';
 export const ERROR_400_MESSAGE = 'Request failed with status code 400'
 export const ERROR_401_MESSAGE = 'Request failed with status code 401'
-export const ERROR_400_TOAST = 'User already logged in!'
+export const ERROR_400 = 'User already logged in!'
 
