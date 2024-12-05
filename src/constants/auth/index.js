544fc502e127d6fb81137f4a236555ab0241a09c
analouMae <aquinoanalou08@gmail.com>
2024-12-05 16:01:16 +0800

    Updated

--- src/constants/auth/index.js
+++ src/constants/auth/index.js
@@ -1,12 +1,14 @@
-export const VALIDATION_CONFIRM_PASSOWRD = 'Passwords do not match.';
+export const VALIDATION_CONFIRM_PASSWORD = 'Passwords do not match.';
 export const VALIDATION_FIRST_NAME = 'Please enter valid first name';
 export const VALIDATION_LAST_NAME = 'Please enter valid last name';
 export const VALIDATION_CONTACT_NUMBER = 'Please enter valid contact number';
 export const VALIDATION_ADDRESS = 'Please enter valid address';
 export const VALIDATION_USERNAME = 'Please enter valid username';
 export const VALIDATION_PASSWORD = 'Please enter valid password';
+
 export const SUCCESS_SIGNUP = 'User registered successfully';
 export const SUCCESS_SIGNIN = 'Successfully logged in!';
+
 export const INVALID_USERNAME = 'Invalid Username';
 export const INVALID_PASSWORD = 'Invalid password';
 export const ERROR_400_MESSAGE = 'Request failed with status code 400'
