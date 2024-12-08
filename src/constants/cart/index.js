e73abc7461b71f2f75dc26b3b47b6f2aabd9596b
Jason Villa <jason.villa@accenture.com>
2024-12-08 20:48:08 +0800

    added constant

---
+++ src/constants/cart/index.js
@@ -0,0 +1,5 @@
+export const CART_STORAGE = 'cart';
+
+export const ADD_TO_CART_SUCCESS_MESSAGE = 'Successfully added to cart';
+export const REMOVE_FROM_CART_SUCCESS_MESSAGE = 'Items removed from cart successfully';
+export const REMOVE_FROM_CART_NONE_ERROR = 'No items selected in the cart';
