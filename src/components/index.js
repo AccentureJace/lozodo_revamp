01cfbc2c088733cb9d06c996d5c5fec78581d550
Jason Villa <jason.villa@accenture.com>
2024-12-10 14:16:25 +0800

    manual resolve

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,5 @@ import { StoreHeader, StoreFooter } from './apps';
 import { ProductBanner, ProductCard } from './product';
 import { CartCard, CartSummary } from './cart';
 import Categories from './categories/Categories';
-import { CartCard, CartSummary } from './cart';
 
-export { ProductBanner, ProductCard, StoreHeader, StoreFooter, Categories, CartCard, CartSummary };
 export { ProductBanner, ProductCard, StoreHeader, StoreFooter, Categories, CartCard, CartSummary };
